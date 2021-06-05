0e5730b0598cae9ab027767c29c87b7f4dfc561e
Rosita311 <64226867+Rosita311@users.noreply.github.com>
2021-06-05 11:40:43 +0200

    fix JS menu changes and mobile header

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-console.log('hi')
-
 const header = document.querySelector('.header');
 const title = document.querySelector('h1');
-const menu = document.querySelector('.menu-item');
+const menu = document.querySelectorAll('.menu-item');
 const button = document.querySelector('.header-button');
 const buttonLink = document.querySelector('.header-button a');
 
 window.onscroll = function() {
     let top = window.scrollY;
-    // console.log(top);
+    console.log(top);
     if(top >= 80) {
         header.classList.add('scroll');
         title.classList.add('scroll');
-        menu.classList.add('scroll');
         button.classList.add('scroll');
         buttonLink.classList.add('scroll');
+        menu.forEach(function(menu) {
+            menu.classList.add('scroll');
+        })
     } else {
         header.classList.remove('scroll');
         title.classList.remove('scroll');
-        menu.classList.remove('scroll');
         button.classList.remove('scroll');
         buttonLink.classList.remove('scroll');
+        menu.forEach(function(menu) {
+            menu.classList.remove('scroll');
+        })
     }
 }
